8b6aae8f515a9aec03eaa79a40b96009a10e6176
unknown <Thomas@LON-C7-7968.students.local>
2023-02-28 15:45:30 +0000

    Waypoints, Patrol & Organisation
    Created the waypoints for the summoner bosses patrol system.
    Also organised some folders to be more readable.

---
+++ Assets/Scripts/Enemy/Bosses/Summoner/SummonerBT.cs
@@ -0,0 +1,14 @@
+using BehaviourTree;
+
+public class SummonerBT : Tree
+{
+    public UnityEngine.Transform[] waypoints;
+
+    public static float speed = 2f;
+    protected override Node SetupTree()
+    {
+        Node root = new TaskPatrol(transform, waypoints);
+
+        return root;
+    }
+}
