d0c339392acd2f5e12add4bcfcfc026ec10a548c
unknown <Thomas@LON-C7-7968.students.local>
2023-03-07 10:35:52 +0000

    Staring audio manager

--- Assets/Scripts/Enemy/Bosses/Summoner/SummonerBT.cs
+++ Assets/Scripts/Enemy/Bosses/Summoner/SummonerBT.cs
@@ -5,6 +5,7 @@ public class SummonerBT : Tree
     public UnityEngine.Transform[] waypoints;
 
     public static float speed = 2f;
+
     protected override Node SetupTree()
     {
         Node root = new TaskPatrol(transform, waypoints);
